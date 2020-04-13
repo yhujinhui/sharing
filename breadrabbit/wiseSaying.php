fbe913f14e5e7fdde6b61848541e674905419d37
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-04-13 22:08:41 +0900

    Add files via upload

--- breadrabbit/wiseSaying.php
+++ breadrabbit/wiseSaying.php
@@ -1,13 +1,4 @@
-<?php
-  $conn = mysqli_connect("localhost","root","mirim2","saying","3307");
 
-  if(!$conn){
-      echo "Faild to connect to MySQl: ".mysqli_connect_error();
-  }else{
-    echo "gljdksfksd";
-  }
-
-?>
 
 <!DOCTYPE html>
 <html lang="ko">
@@ -18,7 +9,42 @@
     <link href="wiseSaying.css" rel="stylesheet">   
   </head>
   <body>
-     
+          <?php
+          $conn = mysqli_connect("localhost","root","mirim2","saying","3307");
+
+          if(!$conn){
+              echo "Faild to connect to MySQl: ".mysqli_connect_error();
+          }
+
+
+          $MAXNUMBER = 5;
+
+
+
+          $number=mt_rand(1, $MAXNUMBER);
+
+          $query = "select contents, source from saying where number=".$number.";";
+          
+          $result = mysqli_query($conn, $query);
+          
+          while($row = mysqli_fetch_array($result)){
+            // echo "</br>".$row['contents']." ,  ".$row['source']."<br/>";
+          ?>
+            <div class="contents">
+              <?php echo $row['contents']; ?>
+            </div>
+            <div class="source">
+              <?php echo "- ".$row['source']." -"; ?>
+            </div>
+
+            
+          
+          <?php
+          }
+
+          
+
+          ?>
     <script src="jsWiseSaying.js"></script>
   </body>
 </html>

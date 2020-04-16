745dd78781ecd73edd67f7b16ed72731ff83b046
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-04-16 22:42:04 +0900

    store 빵배치 & css 수정

--- breadrabbit/store.php
+++ breadrabbit/store.php
@@ -1,13 +1,12 @@
+<!DOCTYPE html>
+</html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <title>빵 굽는 토끼🐰</title>
     <style type = "text/css">
-    @import url("css/introduce.css");
-        </style>
-
-    
+        @import url("css/store.css");
+    </style>
 </head>
 <body>
     <header>
@@ -18,11 +17,83 @@
         <div id="breadcook" onclick="location.href='cookbread.html'"> <p> 빵 굽기 </p>
 </div>
        <div id="logo" onclick="location.href='store.php'"> <img src="images/logo.png" width="10%" heigh="10%"></div>
-
     </header>
-
     <main>
-       <div id="storeback"> <img src="images/store.png" width="100%" height="130%"> </div>
+        <img id="storeimg" src="images/store.png">
+        <div class="container">
+            <div class="row">
+                <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="호기심탐구">
+                    </a>
+                </span>
+                <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="스케치북">
+                    </a>
+                </span>
+                <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="스케치북">
+                    </a>
+                </span>
+                <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="스케치북">
+                    </a>
+                </span>
+                 <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="스케치북">
+                    </a>
+                </span>
+            </div>
+            <div class="row">
+                <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="호기심탐구">
+                    </a>
+                </span>
+                <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="스케치북">
+                    </a>
+                </span>
+                <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="스케치북">
+                    </a>
+                </span>
+                <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="스케치북">
+                    </a>
+                </span>
+                 <span class="item">
+                    <a href="">
+                        <img src="images/bread1.png" class="Categoryimg" alt="스케치북">
+                    </a>
+                </span>
+            </div>
+        </div>
     </main>
+
+<div>
+
+   <!--  <div style="position: absolute;">
+
+        <div style="position: relative; top: 80px; left: 100px;"><img src="images/backbaking.png"></img></div>
+
+    </div>
+
+    <div style="position: absolute;">
+
+        <div style="position: relative; top: 80px; left: 120px;"><img src="images/bread2.png"></img></div>
+
+    </div>
+
+    <img src="images/logo.png"></img> -->
+
+</div>
 </body>
 </html>

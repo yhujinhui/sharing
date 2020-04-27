0a831210419b0c74a64ec1cbd11d617ff1c46c32
JINNNNS <baekhuen@naver.com>
2020-04-27 20:26:04 +0900

    개발자 소개 버튼 추가

--- breadrabbit/store.php
+++ breadrabbit/store.php
@@ -96,6 +96,7 @@ while($row=mysqli_fetch_array($result)){
     <div class="location">
         <a href="<?=$store_href?>">상점 가기</a>
         <a href="<?=$cookbread_href?>">빵 굽기</a>
+        <a href="introduce.html">개발자 소개</a>
         <a href='javascript:void(0);' onClick="top.location='javascript:location.reload()'" id="name"><?=$escaped_name?></a>
         <a href="<?=$log_href?>" id="log" onclick="javascript:return logoutchk();" ><?=$log?></a>
     </div>

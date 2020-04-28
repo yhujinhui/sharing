204141fb6418584275f815c288583e3885935b9b
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-04-28 23:43:02 +0900

    파일 분리

---
+++ breadrabbit/lib/write.php
@@ -0,0 +1,26 @@
+<body>
+    <header>
+        <a href=""></a>
+    </header>
+    <form action="<?=$formhref?>" method="post">
+        <div>
+            <img src="images/bread1.png" id="breadImg">
+        </div>
+        <div class="selectKinds">
+            <select id="selectKinds" name="Kinds" onchange="alert_select_value(this)">
+                <option vlaue="none" disabled selected="selected">빵 종류를 선택하세요</option>
+                <option value="course">진로</option>
+                <option value="family">가족</option>
+                <option value="friend">친구</option>
+                <option value="love">사랑</option>
+                <option value="other">기타</option>
+            </select>
+            <br/>
+        </div>
+        <div>
+            <p>
+                <input type="text" name="breadName" id="breadText" placeholder="빵 이름을 지어주세요" required value="<?=$escaped_title?>">
+            </p>
+            <p>
+                <textarea rows="10" cols="33" name="breadContents" id="breadTextArea" placeholder="고민을 적어주세요" required><?=$escaped_description?></textarea>
+            </p>
--- breadrabbit/update.php
+++ breadrabbit/update.php
@@ -4,7 +4,6 @@ require_once("conn.php");
 $filtered=array(
 	'id'=>mysqli_real_escape_string($conn,$_GET['id']),
     'user_id'=>mysqli_real_escape_string($conn,$_GET['user_id']),
-
 	// content id
 );
 $sql="select * from content where id={$filtered['id']}";
@@ -12,6 +11,8 @@ $result=mysqli_query($conn,$sql);
 $row=mysqli_fetch_array($result);
 $escaped_title=htmlspecialchars($row['title']);
 $escaped_description=htmlspecialchars($row['description']);
+$escaped_kinds=htmlspecialchars($row['kinds']);
+$formhref="update_process.php";
 
 ?>
 <!DOCTYPE html>
@@ -21,41 +22,15 @@ $escaped_description=htmlspecialchars($row['description']);
 	<title>Document</title>
     <link href="css/cookbread.css" rel="stylesheet">   
 </head>
-<body>
-    <header>
-        <a href=""></a>
-    </header>
-    <form action="update_process.php" method="post">
-        <div>
-            <img src="images/bread1.png" id="breadImg">
-        </div>
-        <div class="selectKinds">
-            <select id="selectKinds" name="Kinds" onchange="alert_select_value(this)" required>
-                <option vlaue="none" disabled selected="selected">빵 종류를 선택하세요</option>
-                <option value="course">진로</option>
-                <option value="family">가족</option>
-                <option value="friend">친구</option>
-                <option value="love">사랑</option>
-                <option value="other">기타</option>
-            </select>
-            <br/>
-        </div>
-
-        <div>
-            <p>
-                <input type="text" name="breadName" id="breadText" placeholder="빵 이름을 지어주세요" required value="<?=$escaped_title?>">
-            </p>
-            <p>
-                <textarea rows="10" cols="33" name="breadContents" id="breadTextArea" placeholder="고민을 적어주세요" required><?=$escaped_description?></textarea>
-            </p>
+<?php
+    require_once("lib/write.php");
+?>
             <input type="hidden" name="content_id" value="<?=$filtered['id']?>">
             <input type="hidden" name="user_id" value="<?=$filtered['user_id']?>">
-
-                <input type="submit" id="submitButton" name="submit" alt="빵 굽기!" value="">
+            <input type="submit" id="submitButton" name="submit" alt="빵 굽기!" value="">
         </div>
-
     </form>
 
     <script src="js/cookbread.js"></script>
-	</body>
+</body>
 </html>

ef310b0a9fc43606bb4d2aa3a7d481faec31fd6c
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-04-30 00:30:38 +0900

    알림 띄우는 함수 추가

--- breadrabbit/js/cookbread.js
+++ breadrabbit/js/cookbread.js
@@ -2,6 +2,7 @@ function alert_select_value(selectBread){
     var selected_index = selectBread.selectedIndex; // selectbox에서 선택된 index를 찾음 
     var selected_value = selectBread.options[selected_index].value;     // 선택된 index의 value를 찾음 
 
+    
     if(selected_value=="course"){
         console.log("course 선택됨");
         document.getElementById("breadImg").src = "images/bread1.png";
@@ -18,9 +19,26 @@ function alert_select_value(selectBread){
         console.log("other 선택됨");
         document.getElementById("breadImg").src = "images/bread5.png";
     }    
-};
+}
+
+function chkBreadKinds(){
+    var bread = document.getElementById("selectKinds");
+    var breadvalue = bread.options[bread.selectedIndex].value;
+    console.log("value값 : "+breadvalue);
+
+    if(breadvalue == "none"){        
+        alert("빵 종류를 선택해주세요");
+        return false;
+
+    }else{
+        showWiseSaying();
+        return true;
+    }
+}
+
 
 function showWiseSaying(){
+
     window.open("wiseSaying.php","책 속의 한줄","width=600, height=500, left=100, top=50");
 
 }

278133dcbd428892b56f434cebd729d7750b744e
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-05-24 16:39:23 +0900

    심리 검사 링크 수정 & 연설 추가

--- lib/header_html.php
+++ lib/header_html.php
@@ -9,7 +9,8 @@
   	<a href="">홈</a>
   	<a href="<?=$event_href?>">이벤트</a>
   	<a href="">상담</a>
-  	<a href="trialTest.html">심리검사</a>
+  	<a href="trialTest.php">심리검사</a>
+	<a href="speech.php">연설</a>
   	<a href="<?=$store_href?>">상점 가기</a>
     <a href="<?=$cookbread_href?>" onclick="<?=$logchk?>">빵 굽기</a>
   </div>

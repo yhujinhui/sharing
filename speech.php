2b18549b2e409a4752a5dcb94b0ca683d5dc4823
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-05-24 16:40:53 +0900

    연설 화면 틀 구현

---
+++ speech.php
@@ -0,0 +1,75 @@
+<?php  
+require_once("lib/header_php.php");
+?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>연설</title>
+        <style type = "text/css">
+          @import url("css/header.css");
+        </style>
+    </head>
+    <body>
+        <?php  
+        require_once("lib/header_html.php");
+        ?>
+            <div class="container">
+                <hr style="width: 90%;">
+
+                <div>
+                    <div>
+                        <div> Steven Paul Jobs - 스티브 잡스 </div>
+                        <div>
+                            “Stay Hungry. Stay Foolish.”<br><br>
+                            “항상 갈망하라. 우직하게 나아가라”<br>
+                        </div>
+                    </div>
+                    <div><img src="#"></div>
+                </div>
+
+                <hr style="width: 90%;">
+
+                <div>
+                    <div>
+                        <div> Barack Hussein Obama II  - 버락 오바마 </div>
+                        <div>
+                            “But at the end of the day, the circumstances of your life, what you look like, <br>
+                            where you come from, how much money you have, what you’ve got going on at home, <br>
+                            none of that is an excuse. There is no excuse for not trying.”<br><br>
+                            “그러나 결국 가장 중요한 것은, 인생의 환경, 생김새, 어디서 왔는지, 돈이 얼마나 있는지, 집에서 무슨 일이 벌어지고 있는지, <br>
+                            이들 중 어떠한 것도 변명거리가 되지 않습니다. 시도를 안 하는 것에 대한 변명의 여지 또한 없습니다.”
+
+                        </div>
+                    </div>
+                    <div><img src="#"></div>
+                </div>
+
+                <hr style="width: 90%;">
+
+                <div>
+                    <div>
+                        <div> Les Brown - 레스 브라운 </div>
+                        <div>
+                            He said, “Don't ever say that again. someone’s opinion of you does not have to become your reality.”<br><br>
+                            그는 말했습니다. “그런 말은 다시 하지 말게. 누군가의 의견은 자네의 현실이 되지 않는다네.”
+                        </div>
+                    </div>
+                    <div><img src="#"></div>
+                </div>
+
+                <hr style="width: 90%;">
+
+            </div>
+
+      
+
+
+      <div class="footer">
+        <footer>
+          <span><a href="introduce.html">개발자소개</a></span>
+          <span><a href="">이메일 문의</a></span>
+        </footer>
+      </div>
+    </body>
+</html>

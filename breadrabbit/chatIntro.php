7e3b91c03dc10e72e6c30d9330d7e5f7dc0bf200
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-05-26 22:33:19 +0900

    상담 소개 화면 구현

---
+++ breadrabbit/chatIntro.php
@@ -0,0 +1,43 @@
+<?php  
+require_once("lib/header_php.php");
+?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>상담</title>
+        <style type = "text/css">
+          @import url("css/header.css");
+        </style>
+        <link href="css/chatIntro.css" rel="stylesheet"> 
+    </head>
+    <body>
+        <?php  
+        require_once("lib/header_html.php");
+        ?>
+
+      <div class="container">
+        <div class="Intro">
+            <div class="title">
+                실시간으로 고민을 상담하고 공감 해보세요
+            </div>
+            <div class="content">
+                다양한 사람들에게 고민을 공유하며 새로운 솔루션을 찾아보세요<br>
+                당신의 마음속에 있는 고민을 털어놓을 수 있는 공간입니다
+            </div>
+            <button class="chattingButton">채팅<br>참여하기</button>
+            <div class="content2">
+                익명으로 채팅에 참여할 수 있습니다 
+            </div>
+        </div>
+
+      </div>
+
+      <div class="footer">
+        <footer>
+          <span><a href="introduce.html">개발자소개</a></span>
+          <span><a href="">이메일 문의</a></span>
+        </footer>
+      </div>
+    </body>
+</html>

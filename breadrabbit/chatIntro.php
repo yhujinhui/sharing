a83a9ffd5185883029e84fb9d184f120e42316da
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-05-31 03:24:29 +0900

    상담 화면 버튼 링크 수정

--- breadrabbit/chatIntro.php
+++ breadrabbit/chatIntro.php
@@ -25,7 +25,7 @@ require_once("lib/header_php.php");
                 다양한 사람들에게 고민을 공유하며 새로운 솔루션을 찾아보세요<br>
                 당신의 마음속에 있는 고민을 털어놓을 수 있는 공간입니다
             </div>
-            <button class="chattingButton">채팅<br>참여하기</button>
+            <button class="chattingButton"onclick="window.location.href='chatting.php'" >채팅<br>참여하기</button>
             <div class="content2">
                 익명으로 채팅에 참여할 수 있습니다 
             </div>

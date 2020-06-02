4f2e3035c1a38d2016f78ccf4a40b81b49549470
JINNNNS <baekhuen@naver.com>
2020-06-02 10:25:12 +0900

    심리 테스트 선택하기

---
+++ breadrabbit/ChooseTrialTest.php
@@ -0,0 +1,30 @@
+<?php  
+require_once("lib/header_php.php");
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>심리 검사</title>
+        <link href="css/header.css" rel="stylesheet"> 
+        <link href="css/choosetrialTest.css" rel="stylesheet"> 
+    </head>
+    <body>
+        <?php  
+        require_once("lib/header_html.php");
+        ?>
+            <div>
+                <div class="banner">
+                    
+                </div>
+                <button class="trial_one" onclick="window.location.href='trialTest.php'">성향 알아보기</button>
+                <button class="trial_two" onclick="window.location.href='trialTest2.php'">연애스타일 알아보기</button>
+                <!-- <button class="trial_three" onclick="window.location.href='trialTest3.php'">성격 심리검사</button> -->
+
+                   
+    
+                
+            
+    </body>
+</html>

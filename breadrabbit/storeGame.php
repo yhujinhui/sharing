75f8d875a262c21cfdf6d5da8326a72811af6caf
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-06-21 00:18:21 +0900

    상점 게임 화면 링크 수정

--- breadrabbit/storeGame.php
+++ breadrabbit/storeGame.php
@@ -1,5 +1,7 @@
 <?php  
-require_once("lib/header_php.php");
+//require_once("lib/header_php.php");
+//require_once("lib/logchk.php");
+
 ?>
 <!DOCTYPE html>
 <html lang="ko">
@@ -30,27 +32,27 @@ require_once("lib/header_php.php");
         <div id="fullpage">
             <div class="section">
                 <div class="game1">
+                        <!-- <p class="title">Shop road</p> -->
+                        <!-- <p class="subtext">상점에 방문하면 게임을 통해 빵굽기에 필요한 재료를 획득할 수 있습니다!</p> -->
                     <div class="image">
-                    <a href="store_in_game_1.html"><img class="image1" src="images/store/game1.png" alt="game1"></a>
+                    <a href="store_in_game_1.php"><img class="image1" src="images/store/game1.png" alt="game1"></a>
                     </div>
                 </div>
             </div>
             <div class="section">
                 <div class="game2">
                     <div class="image">
-                    <a href="store_in_game_2.html"><img class="image2" src="images/store/game2.png" alt="game2"></a>
+                    <a href="store_in_game_2.php"><img class="image2" src="images/store/game2.png" alt="game2"></a>
                     </div>                
                 </div>  
             </div>
             <div class="section"> 
                 <div class="game3">
                     <div class="image">
-                    <a href="store_in_game_3.html"><img class="image3" src="images/store/game3.png" alt="game3"></a>
+                    <a href="store_in_game_3.php"><img class="image3" src="images/store/game3.png" alt="game3"></a>
                     </div>                
                 </div>
             </div>       
         </div>
-
-
     </body>
 </html>

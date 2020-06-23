0ab272fa9eb7217679e0d8af743b00e37f60e251
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-06-23 11:18:58 +0900

    상점 화면 css수젖ㅇ

--- breadrabbit/storeGame.php
+++ breadrabbit/storeGame.php
@@ -32,24 +32,31 @@
         <div id="fullpage">
             <div class="section">
                 <div class="game1">
+                    <button class="cookbread" onclick = "location.href = 'cookbread.php' " >빵 구우러 가기</button>
                         <!-- <p class="title">Shop road</p> -->
                         <!-- <p class="subtext">상점에 방문하면 게임을 통해 빵굽기에 필요한 재료를 획득할 수 있습니다!</p> -->
                     <div class="image">
-                    <a href="store_in_game_1.php"><img class="image1" src="images/store/game1.png" alt="game1"></a>
+                        <div class="image1">
+                            <a href="store_in_game_1.php"><img class="image1" src="images/store/game1.png" alt="game1"></a>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="section">
                 <div class="game2">
                     <div class="image">
-                    <a href="store_in_game_2.php"><img class="image2" src="images/store/game2.png" alt="game2"></a>
+                        <div class="image2">
+                            <a href="store_in_game_2.php"><img class="image2" src="images/store/game2.png" alt="game2"></a>
+                        </div>
                     </div>                
                 </div>  
             </div>
             <div class="section"> 
                 <div class="game3">
                     <div class="image">
-                    <a href="store_in_game_3.php"><img class="image3" src="images/store/game3.png" alt="game3"></a>
+                        <div class="image3">
+                            <a href="store_in_game_3.php"><img class="image3" src="images/store/game3.png" alt="game3"></a>
+                        </div>
                     </div>                
                 </div>
             </div>       

2ae3fe8eb27a768fb9368ce78a29bc07100dbd77
JINNNNS <baekhuen@naver.com>
2020-06-24 21:32:28 +0900

    심리검사 pullpage추가

--- breadrabbit/trialTest.php
+++ breadrabbit/trialTest.php
@@ -3,34 +3,42 @@ require_once("lib/header_php.php");
 ?>
 <!DOCTYPE html>
 <html lang="ko">
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>심리 검사</title>
-        <link href="css/header.css" rel="stylesheet"> 
-        <link href="css/trialTest.css" rel="stylesheet"> 
-    </head>
-    <body>
-        <?php  
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compaatible" content="ie=edge"/>
+    <link href="css/header.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="./css/fullpage.min.css"/>
+    <script src="./js/fullpage.min.js"></script>
+    <link href="css/trialTest.css" rel="stylesheet"> 
+
+</head>
+<body>
+
+        <div class="container">
+        
+
+
+<div id="full-page">
+<form name="test" method="post" action="trialTestProc.php" onsubmit="return checksubmit();">
+    <div class="section s0">
+    <?php  
         require_once("lib/header_html.php");
+        
         ?>
-            <div class="container">
-            <div class="box"> <div class="percent">0%</div> <div class="line"> </div> </div>
-
-                <form name="test" method="post" action="trialTestProc.php" onsubmit="return checksubmit();">
-
-                    <div class="one">
-                        <div class="Qone">사람들이 당신 뒷담화하는 걸 들었을 때 어떻게 할건가요?<br><br></div>
-                        <div class="view_1">
+        
+    <div class="Qone">사람들이 당신 뒷담화하는 걸 들었을 때 어떻게 할건가요?<br><br></div>
+    <div class="view_1">
                         <input type="radio" name="Q1" value="A"> 당황스럽고 눈물이 난다 <br><br>
                         <input type="radio" name="Q1" value="B"> 신경 쓰지 않는다 <br><br>
                         <input type="radio" name="Q1" value="C"> 뒷담화 한 사람들을 무시한다 <br><br>
                         <input type="radio" name="Q1" value="D"> 화가 나지만 참는다 <br><br>
                         <input type="radio" name="Q1" value="E"> 가서 따진다
                     </div>
-                    </div>
-                    <div class="two">
-                        <div class="Qtwo">당신은 주로 어떤 모임을 가지나요?<br><br></div>
+    </div>
+    <div class="section s1">
+    <div class="Qtwo">당신은 주로 어떤 모임을 가지나요?<br><br></div>
                         <div class="view_2">
                         <input type="radio" name="Q2" value="A"> 대부분 가족 모임 <br><br>
                         <input type="radio" name="Q2" value="B"> 오랜 친구들 모임 <br><br>
@@ -38,20 +46,19 @@ require_once("lib/header_php.php");
                         <input type="radio" name="Q2" value="D"> 나는 외로운 늑대, 모임이 싫다 <br><br>
                         <input type="radio" name="Q2" value="E"> 새로운 친구 모임
                         </div>
-                    </div>
-    
-                    <div class="three">
-                        <div class="Qthree">모임에서 친구가 같은 옷을 입고 왔습니다. 어떻게 할건가요? <br><br></div>
+    </div>
+    <div class="section s2">
+    <div class="Qthree">모임에서 친구가 같은 옷을 입고 왔습니다. 어떻게 할건가요? <br><br></div>
                         <div class="view_3">
                         <input type="radio" name="Q3" value="A"> 전혀 신경쓰지 않는다  <br><br>
                         <input type="radio" name="Q3" value="B"> 당혹스럽지만 어쩔 수 없다 <br><br>
                         <input type="radio" name="Q3" value="C"> 그냥 내가 옷을 갈아 입는다 <br><br>
                         <input type="radio" name="Q3" value="D"> 그냥 집으로 돌아간다 <br><br>
                         <input type="radio" name="Q3" value="E"> 게임을 해서 한명이 옷을 바꾼다
-                    </div> </div>
-                    
-                    <div class="four">
-                        <div class="Qfour">모임에서 보통 당신의 행동은? <br><br></div>
+                    </div>
+    </div>
+    <div class="section s3">
+    <div class="Qfour">모임에서 보통 당신의 행동은? <br><br></div>
                         <div class="view_4">
 
                         <input type="radio" name="Q4" value="A"> 있는 듯 없는 듯 조용히 있는다 <br><br>
@@ -59,10 +66,10 @@ require_once("lib/header_php.php");
                         <input type="radio" name="Q4" value="C"> 다른 사람 말에 호응을 잘 한다<br><br>
                         <input type="radio" name="Q4" value="D"> 화려한 입담으로 분위기를 주도 <br><br>
                         <input type="radio" name="Q4" value="E"> 리드하고 중심적인 역할
-                    </div> </div>
-    
-                    <div class="five">
-                        <div class="Qfive">다음 중 가장 안 좋은 것은? <br><br></div>
+                    </div>
+    </div>
+    <div class="section s4">
+    <div class="Qfive">다음 중 가장 안 좋은 것은? <br><br></div>
                         <div class="view_5">
 
                         <input type="radio" name="Q5" value="A"> 나는 너무 조용하다 <br><br>
@@ -70,71 +77,75 @@ require_once("lib/header_php.php");
                         <input type="radio" name="Q5" value="C"> 나는 서툴다 <br><br>
                         <input type="radio" name="Q5" value="D"> 나는 불안정하다 <br><br>
                         <input type="radio" name="Q5" value="E"> 나는 완벽하다
-                    </div></div>
-    
-                    <div class="six">
-                        <div class="Qsix">화를 자주내는 편인가요? <br><br></div>
-                        <div class="view_6">
+                    </div>
+    </div>
 
-                        <input type="radio" name="Q6" value="A"> 화를 내는 건 좋지 않아요 <br><br>
-                        <input type="radio" name="Q6" value="B"> 웬만하면 화가 나지 않아요 <br><br>
-                        <input type="radio" name="Q6" value="C"> 가끔 화가 날 때가 있어요 <br><br>
-                        <input type="radio" name="Q6" value="D"> 주변에서 저를 화나게 하네요 <br><br>
-                        <input type="radio" name="Q6" value="E"> 생각만 해도 자주 화가 나요
-                    </div></div>
-    
-                    <div class="seven">
-                        <div class="Qseven">금요일이나 토요일 밤엔 주로 무엇을 하시나요? <br><br></div>
-                        <div class="view_7">
+    <div class="section s5">
+    <div class="view_6">
+    <div class="Qsix">화를 자주내는 편인가요? <br><br></div>
 
-                        <input type="radio" name="Q7" value="A"> 약속이 있을 때만 외출 <br><br>
-                        <input type="radio" name="Q7" value="B"> 무리를 모아 다같이 외출 <br><br>
-                        <input type="radio" name="Q7" value="C"> 뽐내기 위해 그냥 나감 <br><br>
-                        <input type="radio" name="Q7" value="D"> 가급적 집에 있음 <br><br>
-                        <input type="radio" name="Q7" value="E"> 혼자 밖에 나가 어슬렁거림
-                    </div></div>
-    
-                    <div class="eight">
-                        <div class="Qeight">하고 있거나, 앞으로 하고 싶은 운동은 무엇인가요? <br><br></div>
-                        <div class="view_8">
+            <input type="radio" name="Q6" value="A"> 화를 내는 건 좋지 않아요 <br><br>
+            <input type="radio" name="Q6" value="B"> 웬만하면 화가 나지 않아요 <br><br>
+            <input type="radio" name="Q6" value="C"> 가끔 화가 날 때가 있어요 <br><br>
+            <input type="radio" name="Q6" value="D"> 주변에서 저를 화나게 하네요 <br><br>
+            <input type="radio" name="Q6" value="E"> 생각만 해도 자주 화가 나요
+</div>
+    </div>
+
+    <div class="section s6">
+    <div class="view_7">
+    <div class="Qseven">금요일이나 토요일 밤엔 주로 무엇을 하시나요? <br><br></div>
+
+        <input type="radio" name="Q7" value="A"> 약속이 있을 때만 외출 <br><br>
+        <input type="radio" name="Q7" value="B"> 무리를 모아 다같이 외출 <br><br>
+        <input type="radio" name="Q7" value="C"> 뽐내기 위해 그냥 나감 <br><br>
+        <input type="radio" name="Q7" value="D"> 가급적 집에 있음 <br><br>
+        <input type="radio" name="Q7" value="E"> 혼자 밖에 나가 어슬렁거림
+</div>
+    </div>
+
+    <div class="section s7">
+    <div class="view_8">
+                <div class="Qeight">하고 있거나, 앞으로 하고 싶은 운동은 무엇인가요? <br><br></div>
 
                         <input type="radio" name="Q8" value="A"> 테니스나 탁구 <br><br>
                         <input type="radio" name="Q8" value="B"> 볼링, 골프, 야구 <br><br>
                         <input type="radio" name="Q8" value="C"> 조깅, 마라톤 <br><br>
                         <input type="radio" name="Q8" value="D"> 축구나 농구 <br><br>
                         <input type="radio" name="Q8" value="E"> 권투나 격투기
-                    </div></div>
-    
-                    <div class="nine">
-                        <div class="Qnine">당신은 유럽에 도착했어요. 무얼 타고 여행하는 걸 선호하나요?<br><br> </div>
-                        <div class="view_9">
+                    </div>
+    </div>
+
+    <div class="section s8">
+    <div class="view_9">
+    <div class="Qnine">당신은 유럽에 도착했어요. 무얼 타고 여행하는 걸 선호하나요?<br><br> </div>
 
                         <input type="radio" name="Q9" value="A"> 버스 <br><br>
                         <input type="radio" name="Q9" value="B"> 자전거 <br><br>
                         <input type="radio" name="Q9" value="C"> 기차<br><br>
                         <input type="radio" name="Q9" value="D"> 비행기 <br><br>
                         <input type="radio" name="Q9" value="E"> 자동차
-                    </div></div>
-    
-                    <div class="ten">
-                        <div class="Qten">최근 당신 감정과 가장 가까운 것은 무엇인가요?<br><br> </div>
-                        <div class="view_10">
+                    </div>
+    </div>
 
-                        <input type="radio" name="Q10" value="A"> 두려움 <br><br>
-                        <input type="radio" name="Q10" value="B"> 사랑<br><br>
-                        <input type="radio" name="Q10" value="C"> 용기 <br><br>
-                        <input type="radio" name="Q10" value="D"> 분노 <br><br>
-                        <input type="radio" name="Q10" value="E"> 증오
-                    </div></div>
+    <div class="section s9">
+    <div class="view_10">
+    <div class="Qten">최근 당신 감정과 가장 가까운 것은 무엇인가요?<br><br> </div>
 
-                     <input type="submit" value="결과 확인">
+        <input type="radio" name="Q10" value="A"> 두려움 <br><br>
+        <input type="radio" name="Q10" value="B"> 사랑<br><br>
+        <input type="radio" name="Q10" value="C"> 용기 <br><br>
+        <input type="radio" name="Q10" value="D"> 분노 <br><br>
+        <input type="radio" name="Q10" value="E"> 증오
+        </div>
+        <div class="next"><input type="submit" value="결과 확인"> </div>
 
-                </form>
-                <button class="next">다음</button>
-
-            </div>
-
-            <script src="js/trialTest.js"></script>
-
-    </body>
+    </div>
+</form>
+</div>
+    <script>
+    new fullpage('#full-page',{});
+    </script>
+    <script src="js/trialTest.js"></script>
+</body>
 </html>
--- breadrabbit/trialTest2.php
+++ breadrabbit/trialTest2.php
@@ -3,118 +3,128 @@ require_once("lib/header_php.php");
 ?>
 <!DOCTYPE html>
 <html lang="ko">
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>연애스타일 알아보기</title>
-        <link href="css/header.css" rel="stylesheet"> 
-        <link href="css/trialTest2.css" rel="stylesheet"> 
-    </head>
-    <body>
-        <?php  
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compaatible" content="ie=edge"/>
+    <link href="css/header.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="./css/fullpage.min.css"/>
+    <script src="./js/fullpage.min.js"></script>
+    <link href="css/trialTest2.css" rel="stylesheet"> 
+
+</head>
+<body>
+
+        <div class="container">
+        
+
+
+<div id="full-page">
+<form name="test" method="post" action="trialTestProc2.php" onsubmit="return checksubmit();">
+    <div class="section s0">
+    <?php  
         require_once("lib/header_html.php");
+        
         ?>
-            <div class="container">
-            <div class="box"> <div class="percent">0%</div>             <div class="line"></div>
-</div>
-            <div class="line"></div>
-
-                <form name="test" method="post" action="trialTestProc2.php" onsubmit="return checksubmit();">
-                    <div class="one">
-
-                        <div class="Qone">종말이 다가왔습니다. 당신은 딱 한가지의 동물만 살릴 수 있습니다.<br>어떤 동물을 살리실건가요?<br><br></div>
-                        <div class="view_1">
-
+        
+    <div class="Qone">종말이 다가왔습니다. 당신은 딱 한가지의 동물만 살릴 수 있습니다.<br>어떤 동물을 살리실건가요?<br><br></div>
+    <div class="view_1">
                         <input type="radio" name="Q1" value="A"> 토끼 <br><br>
                         <input type="radio" name="Q1" value="B"> 양 <br><br>
                         <input type="radio" name="Q1" value="C"> 사슴 <br><br>
                         <input type="radio" name="Q1" value="D"> 말 <br><br>
                     </div>
-                    </div>
-   
-                    <div class="two">
-                        <div class="Qtwo">당신이 아프리카 여행을 떠나 어떤 부족에 들르게 되었는데, <br>그 부족에서 떠날 때 동물 한마리를 데려가라고 합니다.
+    </div>
+    <div class="section s1">
+    <div class="Qtwo">당신이 아프리카 여행을 떠나 어떤 부족에 들르게 되었는데, <br>그 부족에서 떠날 때 동물 한마리를 데려가라고 합니다.
 어떤 동물을 데려갈 것인가요?<br><br></div>
                         <div class="view_2">
-
                         <input type="radio" name="Q2" value="A"> 원숭이 <br><br>
                         <input type="radio" name="Q2" value="B"> 사자 <br><br>
                         <input type="radio" name="Q2" value="C"> 뱀 <br><br>
                         <input type="radio" name="Q2" value="D"> 기린 <br><br>
-                    </div></div>
-    
-                    <div class="three">
-                        <div class="Qthree">당신이 큰 잘못을 해서, 신이 당신에게 인간이 되기 전에 동물로 먼저 환생하는 벌을 내렸습니다.<br>
-동물을 선택할 수 있는데 어떤 동물로 태어날것인가요?<br><br> </div>
+               
+                        </div>
+    </div>
+    <div class="section s2">
+    <div class="Qthree">당신이 큰 잘못을 해서, 신이 당신에게 인간이 되기 전에 동물로 먼저 환생하는 벌을 내렸습니다.<br>
+동물을 선택할 수 있는데 어떤 동물로 태어날것인가요?<br><br> <br><br></div>
                         <div class="view_3">
-
                         <input type="radio" name="Q3" value="A"> 개  <br><br>
                         <input type="radio" name="Q3" value="B"> 고양이 <br><br>
                         <input type="radio" name="Q3" value="C"> 말 <br><br>
                         <input type="radio" name="Q3" value="D"> 뱀 <br><br>
-                    </div></div>
-                    
-                    <div class="four">
-                        <div class="Qfour">당신에게 한가지 동물을 멸종시킬 수 있는 힘이 있다고 할 때<br>
-어떤 동물을 멸종시킬 건가요?<br><br></div>
+                     
+                    </div>
+    </div>
+    <div class="section s3">
+    <div class="Qfour">당신에게 한가지 동물을 멸종시킬 수 있는 힘이 있다고 할 때<br>
+어떤 동물을 멸종시킬 건가요? <br><br></div>
                         <div class="view_4">
 
                         <input type="radio" name="Q4" value="A"> 사자 <br><br>
                         <input type="radio" name="Q4" value="B"> 뱀 <br><br>
-                        <input type="radio" name="Q4" value="C"> 악어 <br><br>
+                        <input type="radio" name="Q4" value="C"> 악어<br><br>
                         <input type="radio" name="Q4" value="D"> 상어 <br><br>
-                    </div></div>
-    
-                    <div class="five">
-                        <div class="Qfive">어느날 당신이 인간의 말을 할 수 있는 동물을 만났습니다!<br>
-그 동물은 무엇인가요?<br><br> </div>
+                     
+                    </div>
+    </div>
+    <div class="section s4">
+    <div class="Qfive">어느날 당신이 인간의 말을 할 수 있는 동물을 만났습니다!<br>
+그 동물은 무엇인가요?<br><br></div>
                         <div class="view_5">
 
                         <input type="radio" name="Q5" value="A"> 양 <br><br>
                         <input type="radio" name="Q5" value="B"> 말 <br><br>
                         <input type="radio" name="Q5" value="C"> 토끼 <br><br>
                         <input type="radio" name="Q5" value="D"> 새 <br><br>
-                    </div></div>
-    
-                    <div class="six">
-                        <div class="Qsix">당신은 무인도에 고립되었습니다. 동반자로 한가지만 데려갈 수 있습니다. <br>어떤 것을 택할건가요? <br><br></div>
-                        <div class="view_6">
 
-                        <input type="radio" name="Q6" value="A"> 사람 <br><br>
-                        <input type="radio" name="Q6" value="B"> 돼지 <br><br>
-                        <input type="radio" name="Q6" value="C"> 소 <br><br>
-                        <input type="radio" name="Q6" value="D"> 새 <br><br>
-                    </div></div>
-    
-                    <div class="seven">
-                        <div class="Qseven">당신에겐 모든 종류의 동물을 길들일 수 있는 능력이 있습니다.<br>
+                    </div>
+    </div>
+
+    <div class="section s5">
+    <div class="view_6">
+    <div class="Qsix">당신은 무인도에 고립되었습니다. 동반자로 한가지만 데려갈 수 있습니다. <br>어떤 것을 택할건가요? <br><br></div>
+
+            <input type="radio" name="Q6" value="A"> 사람 <br><br>
+            <input type="radio" name="Q6" value="B"> 돼지 <br><br>
+            <input type="radio" name="Q6" value="C"> 소 <br><br>
+            <input type="radio" name="Q6" value="D"> 새 <br><br>
+
+</div>
+    </div>
+
+    <div class="section s6">
+    <div class="view_7">
+    <div class="Qseven">당신에겐 모든 종류의 동물을 길들일 수 있는 능력이 있습니다.<br>
 어떤것을 충성심 있는 애완동물로 만들고싶나요? <br><br></div>
-                        <div class="view_7">
 
-                        <input type="radio" name="Q7" value="A"> 공룡 <br><br>
-                        <input type="radio" name="Q7" value="B"> 백호 <br><br>
-                        <input type="radio" name="Q7" value="C"> 북극곰 <br><br>
-                        <input type="radio" name="Q7" value="D"> 표범 <br><br>
-                    </div></div>
-    
-                    <div class="eight">
-                        <div class="Qeight">당신에게 5분간 특정 동물로 변신할 수 있는 능력이 있다면 어떤것으로 변해보고 싶나요? <br><br></div>
-                        <div class="view_8">
+        <input type="radio" name="Q7" value="A"> 공룡 <br><br>
+        <input type="radio" name="Q7" value="B"> 백호 <br><br>
+        <input type="radio" name="Q7" value="C"> 북극곰 <br><br>
+        <input type="radio" name="Q7" value="D"> 표범 <br><br>
+ 
+</div>
+    </div>
+
+    <div class="section s7">
+    <div class="view_8">
+                <div class="Qeight">당신에게 5분간 특정 동물로 변신할 수 있는 능력이 있다면 어떤것으로 변해보고 싶나요? <br><br></div>
 
                         <input type="radio" name="Q8" value="A"> 사자 <br><br>
                         <input type="radio" name="Q8" value="B"> 고양이 <br><br>
                         <input type="radio" name="Q8" value="C"> 말 <br><br>
                         <input type="radio" name="Q8" value="D"> 비둘기 <br><br>
-                    </div> </div>
-    
+                    </div>
+                    <div class="next"><input type="submit" value="결과 확인"> </div>
+    </div>
 
-                   <input type="submit" value="결과 확인">
-
-                </form>
-                <button class="next">다음</button>
-
-            </div>
-
-            <script src="js/trialTest2.js"></script>
-    </body>
+</form>
+</div>
+    <script>
+    new fullpage('#full-page',{});
+    </script>
+    <script src="js/trialTest2.js"></script>
+</body>
 </html>

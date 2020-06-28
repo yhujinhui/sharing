7da556eeb59ef8cdcc09714a00a7b4b993a38704
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-06-28 12:30:47 +0900

    로고 이미지 링크 수정

--- breadrabbit/lib/header_html.php
+++ breadrabbit/lib/header_html.php
@@ -2,7 +2,7 @@
   <link href="https://fonts.googleapis.com/css?family=Gothic+A1&display=swap" rel="stylesheet">
   
   <div class="nav">
-    <div class="logo"><a href="first.php"><img src="" alt="">로고</a></div>
+    <div class="logo"><a href="first.php"><img width="220" height="60"src="images/logo.PNG" alt=""></a></div>
     <div class="menu">
       <ul>
         <li class="menu-items">
@@ -11,14 +11,14 @@
         <li class="menu-items">
           <a href="ChooseTrialTest.php">심리검사</a>
         </li>
-        <!-- <li class="menu-items">
-          <a href="speech.php">연설</a>
-        </li> -->
         <li class="menu-items">
-          <a href="<?=$store_href?>">상점 가기</a>
+          <a href="speech.php">연설</a>
         </li>
         <li class="menu-items">
-          <a href="<?=$cookbread_href?>" onclick="<?=$logchk?>">빵 굽기</a>
+          <a href="<?=$store_href?>">빵집</a>
+        </li>
+        <li class="menu-items">
+          <a href="<?=$cookbread_href?>" onclick="<?=$logchk?>">빵</a>
         </li>
         <li class="menu-items" style="margin-left: 40px;">
           <a href='account.php' id="name"><?=$escaped_header_name?></a>
